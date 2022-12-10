70ccfa5df8cf194ffbf16c30c36438625ccffb1f
sametdogan51 <120280707+sametdogan51@users.noreply.github.com>
2022-12-11 00:08:35 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   prefix: "/",
-  owner: "690639157013381220",
-  token: "Bot Tokeniniz"
+  owner: "750439565688569887",
+  token: "MTA1MDUxNzQ1NTQ1MTE5NzYxMA.G5j3rD.qtqrQLGC4CcTDGORHspvgNShaGEpBWsmAeiyBk"
 }
